aed2645c99e57382855658fb53560ce4104c272b
Truc Phan <trucphan@my.unt.edu>
2022-08-11 00:09:43 -0500

    create Player object as a factory function and GameTracker object as a module that can update the game board to match each player's symbol in their turn

--- script.js
+++ script.js
@@ -11,22 +11,52 @@ const gameBoard = (() =>{
         squares.appendChild(square);
     }
 
-    const setSquare = (index, mark) => {
-        board[index] = mark;
-    }
+    const setSquare = (index, symbol) => {
+        board[index] = symbol;
+    };
 
     const getSquare = (index) => {
         return board[index];
-    }
+    };
 
     const reset = () => {
         for (let i = 0; i < board.length; i++){
             board[i] = '';
         }
-    }
+    };
 
     return {board ,getSquare, reset, setSquare}
 })();
 
-gameBoard.setSquare(2,'X');
-console.log(gameBoard.board);
+//player object
+const Player = (symbol) => {
+    this.symbol = symbol;
+
+    const getSymbol = () => {
+        return symbol
+    };
+
+    return {getSymbol}
+}
+
+const gameTracker = (() => {
+    const player1 = Player('X');
+    const player2 = Player('O');
+    let count = 0;
+
+    const getCurrentPlayerSymbol = () => {
+        if (count % 2 === 1) return player1.getSymbol()
+        else return player2.getSymbol()
+    };
+
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square, index) => {
+        square.addEventListener('click', () =>{
+            count +=1;
+            gameBoard.setSquare(index, getCurrentPlayerSymbol());
+            square.textContent = getCurrentPlayerSymbol();
+            console.log(gameBoard.board);
+            console.log(count);
+        })
+    })
+})();
