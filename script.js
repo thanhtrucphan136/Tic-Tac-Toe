94ee797b899b7a469b7cce29a0523ea2907b5fe9
Truc Phan <trucphan@my.unt.edu>
2022-08-10 21:16:21 -0500

    create gameBoard object

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 //gameboard object
 const gameBoard = (() =>{
     let board = [];
-
-    for (let i = 0; i < 9; i++){
-        board.push('');
-    }
-
     let squares = document.querySelector('.squares');
 
-    //generate gameboard
-    for (const element of board){
+    //generate gameboard and board array
+    for (let i = 0; i < 9; i++){
+        board.push('');
         const square = document.createElement('div');
         square.classList = 'square';
         squares.appendChild(square);
     }
+
+    const setSquare = (index, mark) => {
+        board[index] = mark;
+    }
+
+    const getSquare = (index) => {
+        return board[index];
+    }
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++){
+            board[i] = '';
+        }
+    }
+
+    return {board ,getSquare, reset, setSquare}
 })();
+
+gameBoard.setSquare(2,'X');
+console.log(gameBoard.board);
